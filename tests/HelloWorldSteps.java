fe926a59bdb075fc4868774cb9eb4db8aa28408e
Arnd Vorrath <73040011+ArndVorrath@users.noreply.github.com>
2023-08-06 18:24:43 +0200

    Update HelloWorldSteps.java

--- tests/HelloWorldSteps.java
+++ tests/HelloWorldSteps.java
@@ -1,40 +1,30 @@
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 import static org.junit.Assert.assertTrue;
 
-public class HelloWorldSteps {
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 
+public class HelloWorldStepDefs {
     private WebDriver driver;
 
-    @Given("I am on the page {string}")
-    public void iAmOnThePage(String url) {
-        // Assuming you have set up the WebDriver executable path properly.
-        // You need to download the WebDriver executable for the specific browser and set its path.
-        System.setProperty("webdriver.chrome.driver", "path_to_chrome_driver");
-
-        driver = new ChromeDriver();
+    @Given("I open the URL {string}")
+    public void iOpenTheURL(String url) {
+        // Set up the WebDriver with ChromeOptions to run headless (no UI)
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+        driver = new ChromeDriver(options);
         driver.get(url);
     }
 
-    @Then("I should see the text {string} on the page")
-    public void iShouldSeeTheTextOnThePage(String expectedText) {
-        // Wait for the page to load (You can implement a proper wait mechanism if needed)
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Find the element that contains the text "Hello World"
-        boolean isTextPresent = driver.findElement(By.xpath("//*[contains(text(),'" + expectedText + "')]")).isDisplayed();
-        
-        // Assert that the text is visible on the page
-        assertTrue("Text '" + expectedText + "' is not visible on the page.", isTextPresent);
-
-        // Close the browser
+    @Then("I should see the text {string}")
+    public void iShouldSeeTheText(String expectedText) {
+        String pageSource = driver.getPageSource();
+        assertTrue("The text \"" + expectedText + "\" was not found on the page.", pageSource.contains(expectedText));
         driver.quit();
     }
 }
